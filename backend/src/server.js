06d2e9a62884328fe3d0f4f4428f72197020482f
DINHCONGMINH <dinhcongminh4424@gmail.com>
2026-03-08 00:21:45 +0700

    build: cấu hình root package.json và chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,25 +3,37 @@ import routes from "./routes/index.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
 // Middleware để phân tích JSON của body của yêu cầu
 app.use(express.json());
-app.use(
-  cors({
-    origin: [process.env.CLIENT_URL, "https://abc.com"],
-    credentials: true,
-  }),
-); // cho phép link url client gọi được api
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: [process.env.CLIENT_URL, "https://abc.com"],
+      credentials: true,
+    }),
+  ); // cho phép link url client gọi được api
+}
 
 // đăng kí các api
 app.use("/api", routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 // kết nối cơ sở dữ liệu
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
